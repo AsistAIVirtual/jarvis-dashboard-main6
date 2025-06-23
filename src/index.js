6ba841c1e18322c27f94d551cfd4a42e16e08d75
AsistAIVirtual <asistaivirtuals@gmail.com>
2025-06-23 14:56:00 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import Dashboard from './Dashboard';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Dashboard />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
